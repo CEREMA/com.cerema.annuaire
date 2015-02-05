9d2734bf789f4ecbfe2b4872a6c2e2eb2eeef0ab
Stéphane Zucatti <stephane.zucatti@me.com>
2015-02-05 11:29:24 +0100

    Update# 25

--- src/Contents/Services/bpclight.js
+++ src/Contents/Services/bpclight.js
@@ -8,27 +8,23 @@
 bpclight = {
 	getFonctions: function(o,cb)
 	{
-		var db=bpclight.using('db');
-		db.model('bpclight','select * from roles order by LibRol',cb);
+		bpclight.using('db').model('bpclight','select * from roles order by LibRol',cb);
 	},
 	getPhoto: function(o,cb)
 	{
-		Agents.using('db').query('bpclight','select trombi from trombi where kage='+o,cb);	
+		bpclight.using('db').query('bpclight','select trombi from trombi where kage='+o,cb);	
 	},	
 	getEtablissements: function(o,cb)
 	{
-		var db=bpclight.using('db');
-		db.model('bpclight','select * from etablissements where archive=0 order by LibEts',cb);		
+		bpclight.using('db').model('bpclight','select * from etablissements where archive=0 order by LibEts',cb);		
 	},
 	getDepartements: function(o,cb)
 	{
-		var db=bpclight.using('db');
-		db.model('bpclight','select * from unites where archive=0 order by LibUni',cb);	
+		bpclight.using('db').model('bpclight','select * from unites where archive=0 order by LibUni',cb);	
 	},
 	getServices: function(o,cb)
 	{
-		var db=bpclight.using('db');
-		db.model('bpclight','select * from subdis where archive=0 order by LibSub ',cb);	
+		bpclight.using('db').model('bpclight','select * from subdis where archive=0 order by LibSub ',cb);	
 	},
 	getGrades: function(o,cb)
 	{
@@ -37,38 +33,11 @@ bpclight = {
 	},
 	getBatiments: function(o,cb)
 	{
-		var db=bpclight.using('db');
-		db.model('bpclight','select * from batiments order by LibBat',cb);	
+		bpclight.using('db').model('bpclight','select * from batiments order by LibBat',cb);	
 	},
 	getCategories: function(o,cb)
 	{
-		var db=bpclight.using('db');
-		db.model('bpclight','select * from catgrad order by LibCgr',cb);		
-	},
-	exportXLS: function(o,cb)
-	{
-		var db=bpclight.using('db');
-		if (o.length==0) {
-			cb(true,null);
-			return;
-		};
-		db.query('bpclight',db.sql("export",{
-			kage: o
-		}),cb);
-	},
-	speech: function(o,cb)
-	{
-		var db=bpclight.using('db');
-		console.log(o);
-		if (!o[2]) cb("NOT_FOUND",null); else {
-			if (o.indexOf('SEARCH')>-1) {
-				if (o.indexOf('subdis')>-1) {
-					o.splice(0,o.indexOf('subdis')+1);
-					console.log('select * from subdis where archive=0 and libsub="'+o.join(' ')+'" order by LibSub');
-					db.model('bpclight','select * from subdis where archive=0 and libsub="'+o.join(' ')+'" order by LibSub',cb);
-				};
-			}
-		}
+		bpclight.using('db').model('bpclight','select * from catgrad order by LibCgr',cb);		
 	}
 };
 
